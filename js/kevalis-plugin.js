032eaf7aa6506d00f954e347b43d430ee6dd8342
mike <scoboria@gmail.com>
2017-07-03 16:56:50 -0400

    Tips popups styled better, more content

--- js/kevalis-plugin.js
+++ js/kevalis-plugin.js
@@ -1,28 +1,32 @@
 ( function( $ ) {
 
     var overlayHTML = '<div id="how-it-works-wrapper">'
-                       + '<div class="closeX">✕</div>'
-                       + '<div class="how-it-works-container">'
+                        + '<div class="closeX">✕</div>'
+                        + '<div class="how-it-works-container">'
                           + '<img src="/wordpress/wp-content/plugins/kevalis-plugin/img/white-arrow-left.png">'
-                             + '<h3 class="how-it-works-number-1">1</h3>'
-                             + '<div style="position: absolute;left: 20px;top: 75px;min-width: 240px;">'
-                                + '<p style="margin-bottom: 0;display: block;">Upload a design,<span style="margin-top: 5px;display: block;">create text, or freehand</span>'
-                       + '</p></div></div></div>';
+                          + '<div class="instruction-1-container">'
+                            + '<h3 class="how-it-works-number-1">1</h3>'
+                            + '<p class="instructions-text">Upload a design,'
+                                + '<span class="instructions-text-line-2">create text, or freehand</span>'
+                            + '</p>'
+                          + '</div>'
+                        + '</div>'
+                          + '<div class="instruction-2-container">'
+                            + '<div class="instructions-text-wrapper">'
+                              + '<img src="/wordpress/wp-content/plugins/kevalis-plugin/img/white-arrow-right.png">'
+                              + '<h3 class="how-it-works-number-2">2</h3>'
+                              + '<p class="instructions-text">Manage layers, undo,'
+                                + '<span> save and more</span>'
+                              + '</p>'
+                          + '</div>'
+                        + '</div>'
+                        + '<div class="quick-tips-wrapper">'
+                          + '<p class="text-top">Quick tips</p>'
+                          + '<p>to get started</p>'
+                        + '</div>'
+                    + '</div>';
 
     $( window ).on("load", function () {
-
-        // var sheet = (function () {
-
-        //     var style = document.createElement("style");
-
-        //     style.appendChild(document.createTextNode(""));
-
-        //     document.head.appendChild(style);
-
-        //     return style.sheet;
-
-        // })();
-
 
         Start();
         window.ImagesModule.imageTipsHelper();
@@ -33,12 +37,10 @@
 
     function loadCssFile(filename) {
         var fileref, filetype = filename.split('.').pop();
-        if (filetype === "css") { //if filename is an external CSS file
+        if (filetype === "css") { 
             fileref = document.createElement("link");
             fileref.setAttribute("rel", "stylesheet");
             fileref.setAttribute("type", "text/css");
-            // fileref.setAttribute("href", filename);
-
             fileref.setAttribute("href", 'http://localhost/wordpress/wp-content/plugins/kevalis-plugin/css/kevalis-styles.css');
         }
         if (typeof fileref != "undefined") {
@@ -51,38 +53,72 @@
 
         function addImageTipsPopup() {
 
-            $('.fpd-content').before('<div class="img-tips-popup" >' +
-                '<p>Helpful Tips Here</p> '
+            $('.fpd-content').after('<div id="tips-popup">' +
+                '<p>Helpful Text Tips Here</p> '
+                + '<div class="closeX">✕</div>'
                 + '</div>');
+
+            $('#tips-popup > .closeX').on('click', toggleImageTips);
 
         }
 
         function toggleImageTips() {
-            if($('.img-tips-popup').css('display') === 'block') {
-                $('.img-tips-popup').css('display', 'none');
+
+            // update content on popup for the tab
+            updateTipsPopupContent();
+
+            if($('#tips-popup').css('display') === 'block') {
+                $('#tips-popup').css('display', 'none');
             } else {
-                $('.img-tips-popup').css('display', 'block');
+                $('#tips-popup').css('display', 'block');
             }
         }
 
         function addImageTips() {
             $('.fpd-content').before(
-                '<div class="img-info-link">' +
-                    '<p>Image Tips</p> '
-                    + '</div>' );
+                '<div class="img-info-link">'
+                    + '<p class="tips-link">Text Tips</p>'
+                    + '</div>'
+            );
 
             $('.img-info-link').on('click', toggleImageTips);
         }
 
-
-        function toggleImageTipsLink() {
+        function updateTipsLinkText() {
             $('div[data-module]').on('click', function () {
-
-                if ($(this).attr("data-module") === 'images') {
-                    $(".img-info-link").css('display', 'block');
-                } else {
-                    $(".img-info-link").css('display', 'none');
+                var module = $(this).attr("data-module");
+                if ( module === 'images') {
+                    $(".tips-link").text('Image Tips');
+                } else if (module === "text") {
+                    $(".tips-link").text('Text Tips');
+                } else if (module === "designs") {
+                    $(".tips-link").text('Custom Design Tips');
+                } else if (module === "names-numbers") {
+                    $(".tips-link").text('Names & names-numbers Tips');
+                } else if (module === "drawing") {
+                    $(".tips-link").text('Drawing Tips');
                 }
+            });
+        }
+
+        function setPopupContent(module) {
+            if (module === 'images') {
+                $("#tips-popup p").text('Helpful Image Tips');
+            } else if (module === "text") {
+                $("#tips-popup p").text('Helpful Text Tips');
+            } else if (module === "designs") {
+                $("#tips-popup p").text('Helpful Custom Design Tips');
+            } else if (module === "names-numbers") {
+                $("#tips-popup p").text('Helpful Names & names-numbers Tips');
+            } else if (module === "drawing") {
+                $("#tips-popup p").text('Helpful Drawing Tips');
+            }
+        }
+
+        function updateTipsPopupContent() {
+            $('div[data-module]').on('click', function () {
+                var module = $(this).attr("data-module");
+                setPopupContent(module);
             });
         }
 
@@ -95,13 +131,7 @@
             });
         }
 
-        function addShowTipsOverlayLink() {
-            $('.fpd-product-designer-wrapper').before('<div><h1>Custon Design</h1><h2 class="tips-overlay-link">How It Works</h2></div>');
-            $('.tips-overlay-link').on('click', toggleOverlay);
-        };
-
-
-        var toggleOverlay = function() {
+        var toggleOverlay = function () {
             if($('#how-it-works-wrapper').css('visibility') === 'visible') {
                 $('#how-it-works-wrapper').css('visibility', 'hidden');
             } else {
@@ -109,13 +139,17 @@
             }
         };
 
+        function addShowTipsOverlayLink() {
+            $('.fpd-product-designer-wrapper').before('<div>'
+                + '<h1>Custom Design</h1><h2 class="tips-overlay-link">How It Works</h2>'
+                + '</div>');
+            $('.tips-overlay-link').on('click', toggleOverlay);
+        };
 
         window.ImagesModule.imageTipsHelper = function () {
-
             addImageTips();
-            toggleImageTipsLink();
+            updateTipsLinkText();
             addImageTipsPopup();
-
         };
 
         window.FancyProductDesigner.overlayHelper = function () {
@@ -126,8 +160,3 @@
     }
 
 }) ( jQuery );
-
-
-
-
-
