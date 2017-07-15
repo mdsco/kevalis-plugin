8c7b234e4c82864761227e1f65a47908a0a88cb5
mike <scoboria@gmail.com>
2017-07-15 19:40:51 -0400

    Minified js and css

--- kavalis-plugin.php
+++ kavalis-plugin.php
@@ -7,15 +7,23 @@ Author URI:
 Version: 1.0.0
 */
 
-function kavalis_plugin_script_enqueue(){
 
-	wp_register_style('Kavalis Style', plugins_url( '/css/kavalis-styles.css', __FILE__ ), array(), '1.0.0', 'all');
+function kavalis_register(){
 
-	wp_enqueue_style('Kavalis Style');
+	wp_register_style('kavalis-style', plugins_url( '/css/kavalis-styles-min.css', __FILE__ ), array(), '1.0.0', 'all');
+    wp_register_script( 'kevalis-script', plugins_url( '/js/kavalis-plugin-min.js', __FILE__ ), array('jquery'));
 
-    wp_register_script( 'Kavalis Script', plugins_url( '/js/kavalis-plugin.js', __FILE__ ), array('jquery') );
-
-	wp_enqueue_script('Kavalis Script');
 }
 
+function kavalis_plugin_script_enqueue(){
+	wp_enqueue_style('kavalis-style');
+}
+
+function footer_handler() {
+	wp_enqueue_script( 'kevalis-script' );
+}
+
+
+add_action( 'init', 'kavalis_register', 30 );
 add_action('wp_enqueue_scripts', 'kavalis_plugin_script_enqueue');
+add_action( 'wp_footer', 'footer_handler' );
